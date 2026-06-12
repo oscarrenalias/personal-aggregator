9e1b8a12b4e876116d5b96da13f704a67e501ad0
Renalias, Oscar <oscar@renalias.net>
2026-06-12 19:54:48 +0300

    [takt] B-661c2716: Clusterer: package skeleton and config

---
+++ packages/aggregator-clusterer/src/aggregator_clusterer/worker.py
@@ -0,0 +1,9 @@
+from aggregator_clusterer.config import ClustererSettings
+
+
+def run(settings: ClustererSettings) -> None:
+    raise NotImplementedError("Clusterer worker not yet implemented")
+
+
+def run_once(settings: ClustererSettings) -> None:
+    raise NotImplementedError("Clusterer worker not yet implemented")
