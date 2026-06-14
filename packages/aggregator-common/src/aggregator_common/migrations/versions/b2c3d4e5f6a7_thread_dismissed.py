08d4c51db6c7aa0b3865f7f2fd440221a47c9ccc
Renalias, Oscar <oscar@renalias.net>
2026-06-14 21:36:31 +0300

    [takt] B-b4f358da: Dev: Add dismissed column to Thread ORM model and Alembic migration

---
+++ packages/aggregator-common/src/aggregator_common/migrations/versions/b2c3d4e5f6a7_thread_dismissed.py
@@ -0,0 +1,28 @@
+"""Add dismissed column to threads table
+
+Revision ID: b2c3d4e5f6a7
+Revises: a8b9c0d1e2f3
+Create Date: 2026-06-14 00:00:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision: str = "b2c3d4e5f6a7"
+down_revision: Union[str, None] = "a8b9c0d1e2f3"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "threads",
+        sa.Column("dismissed", sa.Boolean(), nullable=False, server_default="false"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("threads", "dismissed")
